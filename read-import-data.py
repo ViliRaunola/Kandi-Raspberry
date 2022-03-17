0fbcb9471025b0963674975a934b71c26ea4a143
Vili Raunola <raunolavili@gmail.com>
2022-03-17 13:42:31 +0200

    Changed wifi file parse to airodump, timer for reading

--- read-import-data.py
+++ read-import-data.py
@@ -1,43 +1,73 @@
+from os import read
 import sys
 import json
 import requests
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+import time
 
 
-
-def sendDataToServer(file_name, url_to_save):
-    
+def readWifiFile(file_name):
     #Open the file for reading
     f_wifi = open(file_name, 'r')
     
-    #Reading the header line
+    #Reading the header & first line off
     next(f_wifi)
-
-    #macAddr 0,vendor 1,SSID 2,Security 3,Privacy 4,Channel 5,Frequency 6,Signal Strength 7,Strongest Signal Strength 8,Bandwidth 9,Last Seen 10,First Seen 11,GPS Valid,Latitude,Longitude,Altitude,Speed,Strongest GPS Valid,Strongest Latitude,Strongest Longitude,Strongest Altitude,Strongest Speed
+    next(f_wifi)
 
     data_list = []
 
-    #Saving information from listening file to an object called data
+    #First half of the file, containing APs   
     for line in f_wifi:
+        line = line.replace('\n', '').replace('\t', '')
+        if line == '':
+            break
+        line_array = line.split(',')
+        #Station MAC 0, First time seen 1, Last time seen 2, Power 3, # packets 4, BSSID 5, Probed ESSIDs 6
+        data = {
+            "MAC_Address": line_array[0],
+            "First_Seen": line_array[1],
+            "Last_Seen": line_array[2],
+            "Signal_Strength": line_array[8],
+            "ESSID": line_array[13],
+        }
+        #BSSID 0, First time seen 1, Last time seen 2, channel 3, Speed 4, Privacy 5, Cipher 6, Authentication 7, Power 8, # beacons 9, # IV 10, LAN IP 11, ID-length 12, ESSID 13, Key 14 
+        data_list.append(data)
+    
+    #Reading the header off
+    next(f_wifi)
+
+    #Second half of the file, containing clients
+    for line in f_wifi: 
+        line = line.replace('\n', '').replace('\t', '')
+        if line == '':
+            break
         line_array = line.split(',')
         data = {
             "MAC_Address": line_array[0],
-            "Vendor": line_array[1],
-            "SSID": line_array[2],
-            "Channel": line_array[5],
-            "Freaquency": line_array[6],
-            "Signal_Strength": line_array[7],
-            "Last_Seen": line_array[10],
-            "First_Seen": line_array[11],
+            "First_Seen": line_array[1],
+            "Last_Seen": line_array[2],
+            "Signal_Strength": line_array[3],
+            "BSSID": line_array[5],
+            "Probed_ESSID": line_array[6]
         }
-        
         data_list.append(data)
-
-    #print(data_list)
-
-    r = requests.post(url_to_save, json=data_list)
-    print(r.json())
+    
+    return data_list
+       
+def sendDataToServer(file_name, url_to_save, timer):
+    
+    while True:
+        try:
+            data_list = readWifiFile(file_name)
+            r = requests.post(url_to_save, json=data_list)
+            r = r.json()
+            if r.get('success'):
+                print('Data sent to server.')
+            else:
+                print('Sending the data failed.')
+            time.sleep(timer)
+        except KeyboardInterrupt:
+            print('Shutting down...')
+            break
         
 
 
@@ -45,8 +75,10 @@ def sendDataToServer(file_name, url_to_save):
 def main(argv):
 
     url_to_save = 'http://localhost:1234/api/save'
-    file_name = "raspberrypi_wifi_2022-03-16_14_16_45.csv"
+    file_name = "testi-01.csv"
+    timer_for_reading_sending = 5
 
+    #TODO Lisää ohjelmien käynnistys!!!
 
     for arg in argv:
         if arg == "-help":
@@ -54,7 +86,7 @@ def main(argv):
             sys.exit(0)
         if arg == "-web":
             print("Sending to web")
-            sendDataToServer(file_name, url_to_save)
+            sendDataToServer(file_name, url_to_save, timer_for_reading_sending)
 
 
 if __name__ == "__main__":
